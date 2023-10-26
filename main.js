cc1d6253119d6e1313626784be0ff98036863b64
Cheshire <146116556+CheshireLv@users.noreply.github.com>
2023-10-26 13:11:03 -0300

    Add files via upload

---
+++ main.js
@@ -0,0 +1,42 @@
+const header = document.querySelector("header");
+
+window.addEventListener ("scroll", function() {
+    header.classList.toggle ("Sticky", window.scrollY > 0);
+});
+
+const slider = document.querySelectorAll('.slider');
+const btnPrev = document.getElementById('prev-button');
+const btnNext = document.getElementById('next-button');
+
+let currentSlide = 0;
+
+function hideSlider() {
+slider.forEach(item => item.classList.remove('on'))
+}
+
+function showSlider() {
+slider[currentSlide].classList.add('on')
+}
+
+function nextSlider() {
+hideSlider()
+if(currentSlide === slider.length -1) {
+    currentSlide = 0
+} else {
+    currentSlide++
+}
+showSlider()
+}
+
+function prevSlider() {
+hideSlider()
+if(currentSlide === 0) {
+    currentSlide = slider.length -1
+} else {
+    currentSlide--
+}
+showSlider()
+}
+
+btnNext.addEventListener('click', nextSlider)
+btnPrev.addEventListener('click', prevSlider)
